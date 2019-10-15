22b385f18f13287fac26d5206b732e7170bdf06a
saurabhjangid2000 <31413264+saurabhjangid2000@users.noreply.github.com>
2019-10-16 01:59:03 +0530

    Create index.js

---
+++ part2/PhoneBook/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import './index.css'
+import Notification from './components/Notification'
+
+ReactDOM.render(<App />, document.getElementById("root"))
